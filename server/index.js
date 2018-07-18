923f29ce29465f8b55d87810aae531644def24b2
Brendon Kuleck <brendonkuleck@gmail.com>
2018-07-18 13:15:18 -0500

    Super simple server

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ const http = require('http');
 const server = http.createServer();
 
 server.on('request', (req, res) => {
-  res.write("Hello Node!\n");
+
 
   setTimeout(() => {
     res.write("Still on...");
