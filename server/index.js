97b1709b6cc11ebfca064a750509c60e0a307b9f
Brendon Kuleck <brendonkuleck@gmail.com>
2018-07-18 12:27:35 -0500

    Hello Node

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ const http = require('http');
 const server =  http.createServer();
 
 server.on('request', (req, res) => {
-  res.write("Hello Node!");
+  res.write("Hello Node!\n");
   res.end();
 });
 
