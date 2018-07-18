704ae94edc448349ec1513db44d745fdfc760641
Brendon Kuleck <brendonkuleck@gmail.com>
2018-07-18 16:17:42 -0500

    Switching to class based

--- client/components/App.js
+++ client/components/App.js
@@ -1,7 +1,17 @@
-const App = () => {
-  return (
-    <h2>Hello components</h2>
-  );
-};
+// Presentational
+
+// const App = () => {
+//   return (
+//     <h2>Hello components</h2>
+//   );
+// };
+
+class App extends React.Component {
+  render(){
+    return (
+      <h2>Hello class-based components</h2>
+    )
+  }
+}
 
 export default App;
