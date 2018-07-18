0ec34919d17078f7f0bb4843d78a5df772b72c44
Brendon Kuleck <brendonkuleck@gmail.com>
2018-07-18 16:12:20 -0500

    Super basic component

---
+++ client/components/App.js
@@ -0,0 +1,7 @@
+const App = () => {
+  return (
+    <h2>Hello components</h2>
+  );
+};
+
+export default App;
--- client/index.js
+++ client/index.js
@@ -1,5 +1,7 @@
+import App from './components/App';
+
 ReactDOM.render(
   // React.createElement("h2", null, "Hello React"),
-  <h2>Hello JSX</h2>,
+  <App />,
   document.getElementById("root")
 )
