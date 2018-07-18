254a96eef2176e50c9eb1f52bdffda9799570150
Brendon Kuleck <brendonkuleck@gmail.com>
2018-07-18 15:20:51 -0500

    Hello webpack

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+const path = require('path');
+
+module.exports = {
+  entry: './client/index.js',
+  output: {
+    path: path.join(__dirname, 'client'),
+    filename: 'bundle.js'
+  },
+  mode: 'none',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      }
+    ]
+  }
+};
